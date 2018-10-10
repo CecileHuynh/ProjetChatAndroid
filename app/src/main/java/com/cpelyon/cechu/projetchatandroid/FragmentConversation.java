a2e74867d60751a7b650cdd94938e52d8641fb6f
tristan <tristan.gueneau@cpe.fr>
2018-10-10 22:54:57 +0200

    Ajout des Fragments

---
+++ app/src/main/java/com/cpelyon/cechu/projetchatandroid/FragmentConversation.java
@@ -0,0 +1,22 @@
+package com.cpelyon.cechu.projetchatandroid;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentConversation extends Fragment {
+    View view;
+    public FragmentConversation(){}
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.conversations_fragment,container,false);
+        return view;
+    }
+}
--- app/src/main/java/com/cpelyon/cechu/projetchatandroid/HomePageUser.java
+++ app/src/main/java/com/cpelyon/cechu/projetchatandroid/HomePageUser.java
@@ -1,16 +1,45 @@
 package com.cpelyon.cechu.projetchatandroid;
 
-import android.os.Bundle;
+import android.content.Intent;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.TabItem;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
-public class HomePageUser extends AppCompatActivity{
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class HomePageUser extends AppCompatActivity {
+
+
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
+    private AppBarLayout appBarLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page_user);
+        tabLayout = (TabLayout) findViewById(R.id.tablayout_id);
+        appBarLayout = (AppBarLayout) findViewById(R.id.appbarid);
+        viewPager = (ViewPager) findViewById(R.id.viewpager_id);
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+
+        //Ajout des fragments
+        adapter.AddFragment(new FragmentContact(),"Contacts");
+        adapter.AddFragment(new FragmentConversation(),"Conversations");
+
+        //Configuration de l'Adapter
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
+
 
     }
 
 
 }
+
